39d7361df83c7f41aaace04fafe1f3c90f4d84b1
jbreger <jimmy.tiger@gmail.com>
2012-08-24 12:05:51 -0400

    Changed the way tables work
    Changed the way tables work.  Need to update code to deal with new data
    structure.
    
    Check in statement.

--- luabbio.lua
+++ luabbio.lua
@@ -5,90 +5,134 @@ OUTPUT="OUTPUT"
 INPUT="INPUT"
 pinList={} --needed for unexport
 startTime=os.clock() --needed for milliseconds
-digitalPinDef={"P8.3"=38,"P8.4"=39,"P8.5"=34,"P8.6"=35,"P8.11"=45,"P8.12"=44,"P8.14"=26,"P8.15"=47,"P8.16"=46,"P8.17"=27,"P8.18"=65,"P8.20"=63,"P8.21"=62,"P8.22"=37,"P8.23"=36,"P8.24"=33,"P8.25"=32,"P8.26"=61,"P8.27"=86,"P8.28"=88,"P8.29"=87,"P8.30"=89,"P8.39"=76,"P8.40"=77,"P8.41"=74,"P8.42"=75,"P8.43"=72,"P8.44"=73,"P8.45"=70,"P8.46"=71,"P9.12"=60,"P9.15"=48,"P9.23"=117,"P9.27"=115,"P9.42"=7}
 
+--Refer to pages 55,57,60,62 in Beagle Bone reference manual
+digitalPinsdef={
+--pin is pin p_number refers to pin numbers
+{pin="P8.3", p_number=38},
+{pin="P8.4", p_number=39},
+{pin="P8.5", p_number=34},
+{pin="P8.6", p_number=35},
+{pin="P8.11", p_number=45},
+{pin="P8.12", p_number=44},
+{pin="P8.14", p_number=26},
+{pin="P8.15", p_number=47},
+{pin="P8.16", p_number=46},
+{pin="P8.17", p_number=27},
+{pin="P8.18", p_number=65},
+{pin="P8.20", p_number=63},
+{pin="P8.21", p_number=62},
+{pin="P8.22", p_number=37},
+{pin="P8.23", p_number=36},
+{pin="P8.24", p_number=33},
+{pin="P8.25", p_number=32},
+{pin="P8.26", p_number=61},
+{pin="P8.27", p_number=86},
+{pin="P8.28", p_number=88},
+{pin="P8.29", p_number=87},
+{pin="P8.30", p_number=89},
+{pin="P8.39", p_number=76},
+{pin="P8.40", p_number=77},
+{pin="P8.41", p_number=74},
+{pin="P8.42", p_number=75},
+{pin="P8.43", p_number=72},
+{pin="P8.44", p_number=73},
+{pin="P8.45", p_number=70},
+{pin="P8.46", p_number=71},
+{pin="P9.12", p_number=60},
+{pin="P9.15", p_number=48},
+{pin="P9.23", p_number=117},
+{pin="P9.27", p_number=115},
+{pin="P9.42", p_number=7}
+}
+
+
+--pin is pin mux_value refers to mux values
 pinMuxDef = {
-"P8.3"=	"gpmc_ad6",
-"P8.4"=	"gpmc_ad7",
-"P8.5"=	"gpmc_ad2",
-"P8.6"=	"gpmc_ad3",
-"P8.7"=	"gpmc_advn_ale",
-"P8.8"=	"gpmc_oen_ren",
-"P8.9"=	"gpmc_ben0_cle",
-"P8.10"="gpmc_wen",
-"P8.11"="gpmc_ad13",
-"P8.12"="gpmc_ad12",
-"P8.13"="gpmc_ad9",
-"P8.14"="gpmc_ad10",
-"P8.15"="gpmc_ad15",
-"P8.16"="gpmc_ad14",
-"P8.17"="gpmc_ad11",
-"P8.18"="gpmc_clk",
-"P8.19"="gpmc_ad8",
-"P8.20"="gpmc_csn2",
-"P8.21"="gpmc_csn1",
-"P8.22"="gpmc_ad5",
-"P8.23"="gpmc_ad4",
-"P8.24"="gpmc_ad1",
-"P8.25"="gpmc_ad0",
-"P8.26"="gpmc_csn0",
-"P8.27"="lcd_vsync",
-"P8.28"="lcd_pclk",
-"P8.29"="lcd_hsync",
-"P8.30"="lcd_ac_bias_en",
-"P8.31"="lcd_data14",
-"P8.32"="lcd_data15",
-"P8.33"="lcd_data13",
-"P8.34"="lcd_data11",
-"P8.35"="lcd_data12",
-"P8.36"="lcd_data10",
-"P8.37"="lcd_data8",
-"P8.38"="lcd_data9",
-"P8.39"="lcd_data6",
-"P8.40"="lcd_data7",
-"P8.41"="lcd_data4",
-"P8.42"="lcd_data5",
-"P8.43"="lcd_data2",
-"P8.44"="lcd_data3",
-"P8.45"="lcd_data0",
-"P8.46"="lcd_data1",
-"P9.11"="gpmc_wait0",
-"P9.12"="gpmc_ben1",
-"P9.13"="gpmc_wpn",
-"P9.14"="gpmc_a2",
-"P9.15"="gpmc_a0",
-"P9.16"="gpmc_a3",
-"P9.17"="spi0_cs0",
-"P9.18"="spi0_d1",
-"P9.19"="uart1_rtsn",
-"P9.20"="uart1_ctsn",
-"P9.21"="spi0_d0",
-"P9.22"="spi0_sclk",
-"P9.23"="gpmc_a1",
-"P9.24"="uart1_txd",
-"P9.25"="mcasp0_ahclkx",
-"P9.26"="uart1_rxd",
-"P9.27"="mcasp0_fsr",
-"P9.28"="mcasp0_ahclkr",
-"P9.29"="mcasp0_fsx",
-"P9.30"="mcasp0_axr0",
-"P9.31"="mcasp0_ahclkx",
-"P9.41"="xdma_event_intr0",
-"P9.42"="ecap0_in_pwm0_out"}
+{pin="P8.3", mux_value="gpmc_ad6"},
+{pin="P8.4", mux_value="gpmc_ad7"},
+{pin="P8.5", mux_value="gpmc_ad2"},
+{pin="P8.6", mux_value="gpmc_ad3"},
+{pin="P8.7", mux_value="gpmc_advn_ale"},
+{pin="P8.8", mux_value="gpmc_oen_ren"},
+{pin="P8.9", mux_value="gpmc_ben0_cle"},
+{pin="P8.10", mux_value="gpmc_wen"},
+{pin="P8.11", mux_value="gpmc_ad13"},
+{pin="P8.12", mux_value="gpmc_ad12"},
+{pin="P8.13", mux_value="gpmc_ad9"},
+{pin="P8.14", mux_value="gpmc_ad10"},
+{pin="P8.15", mux_value="gpmc_ad15"},
+{pin="P8.16", mux_value="gpmc_ad14"},
+{pin="P8.17", mux_value="gpmc_ad11"},
+{pin="P8.18", mux_value="gpmc_clk"},
+{pin="P8.19", mux_value="gpmc_ad8"},
+{pin="P8.20", mux_value="gpmc_csn2"},
+{pin="P8.21", mux_value="gpmc_csn1"},
+{pin="P8.22", mux_value="gpmc_ad5"},
+{pin="P8.23", mux_value="gpmc_ad4"},
+{pin="P8.24", mux_value="gpmc_ad1"},
+{pin="P8.13", mux_value="gpmc_ad9"},
+{pin="P8.25", mux_value="gpmc_ad0"},
+{pin="P8.26", mux_value="gpmc_csn0"},
+{pin="P8.27", mux_value="lcd_vsync"},
+{pin="P8.28", mux_value="lcd_pclk"},
+{pin="P8.29", mux_value="lcd_hsync"},
+{pin="P8.30", mux_value="lcd_ac_bias_en"},
+{pin="P8.31", mux_value="lcd_data14"},
+{pin="P8.32", mux_value="lcd_data15"},
+{pin="P8.33", mux_value="lcd_data13"},
+{pin="P8.34", mux_value="lcd_data11"},
+{pin="P8.35", mux_value="lcd_data12"},
+{pin="P8.36", mux_value="lcd_data10"},
+{pin="P8.37", mux_value="lcd_data8"},
+{pin="P8.38", mux_value="lcd_data9"},
+{pin="P8.39", mux_value="lcd_data6"},
+{pin="P8.40", mux_value="lcd_data7"},
+{pin="P8.41", mux_value="lcd_data4"},
+{pin="P8.42", mux_value="lcd_data5"},
+{pin="P8.43", mux_value="lcd_data2"},
+{pin="P8.44", mux_value="lcd_data3"},
+{pin="P8.45", mux_value="lcd_data0"},
+{pin="P8.46", mux_value="lcd_data1"},
+{pin="P9.11", mux_value="gpmc_wait0"},
+{pin="P9.12", mux_value="gpmc_ben1"},
+{pin="P9.13", mux_value="gpmc_wpn"},
+{pin="P9.14", mux_value="gpmc_a2"},
+{pin="P9.15", mux_value="gpmc_a0"},
+{pin="P9.16", mux_value="gomc_a3"},
+{pin="P9.17", mux_value="spio=0_cs0"},
+{pin="P9.18", mux_value="spi0_d1"},
+{pin="P9.19", mux_value="uart1_rtsn"},
+{pin="P9.20", mux_value="uart1_ctsn"},
+{pin="P9.21", mux_value="spi0_d0"},
+{pin="P9.22", mux_value="spi0_sclk"},
+{pin="P9.23", mux_value="gpmc_a1"},
+{pin="P9.24", mux_value="uart1_txd"},
+{pin="P9.25", mux_value="mcasp0_ahclkx"},
+{pin="P9.26", mux_value="uart1_rxd"},
+{pin="P9.27", mux_value="mcasp0_fsr"},
+{pin="P9.28", mux_value="mcasp0_ahclkr"},
+{pin="P9.29", mux_value="mcasp0_fsx"},
+{pin="P9.30", mux_value="mcasp0_aaxr0"},
+{pin="P9.41", mux_value="xdma_event_intr0"},
+{pin="P9.42", mux_value="ecap0_in_pwm0_out"},
+}
 
+--pin is pin number av is analog value
 analogPinDef={
-	"P9.33"="ain4",
-	"P9.35"="ain6",
-	"P9.36"="ain5",
-	"P9.37"="ain2",
-	"P9.38"="ain3",
-	"P9.39"="ain0",
-	"P9.40"="ain1"
+	{pin="P9.33", av="ain4"},
+	{pin="P9.35", av="ain6"},
+	{pin="P9.36", av="ain5"},
+	{pin="P9.37", av="ain2"},
+	{pin="P9.38", av="ain3"},
+	{pin="P9.39", av="ain0"},
+	{pin="P9.40", av="ain1"},
 	}
+--check pins
 
 function pinMode(pin,directions)
-
-	if pin in digitalPinDef then
+	-- problem with in statement
+	if (pin in digitalPinDef) then
 		fw=file("/sys/class/gpio/export", "w")
 		fw.write("%d" % (digitalPinDef[pin]))
 		fw.close()
@@ -110,12 +154,13 @@ function pinMode(pin,directions)
 	else
 		print "pinMode error: pin " + pin + " is not defined as a digital I/O pin in the pin definition."
 	end
-	end
-
+end
+end
 end
 
 
-end
+
+--check pins
 
 function digitalWrite(pin,status)
 	--digitalWrite(pin, status) sets a pin HIGH or LOW
@@ -133,7 +178,7 @@ function digitalWrite(pin,status)
 		print "digitalWrite error: Pin mode for " + pin + " has not been set. Use pinMode(pin, INPUT) first."
 end
 end
-
+--check pins
 function digitalRead(pin)
 	--digitalRead(pin) returns HIGH or LOW for a given pin.
 	if digitalPinDef[pin] in pinList
@@ -153,7 +198,7 @@ function digitalRead(pin)
 	end
 end
 
-
+--check pins
 function analogRead(pin) --analogRead(pin) returns analog value for a given pin.
 	if pin in analogPinDef then
 		fileName="/sys/devices/platform/tsc/" + (analogPinDef[pin])
@@ -167,6 +212,7 @@ function analogRead(pin) --analogRead(pin) returns analog value for a given pin.
 	end
 
 function pinUnexport(pin) --helper function for cleanup()
+--check pin functions
 	fw=file("/sys/class/gpio/unexport", "w")
 	fw.write("%d" % (pin))
 	fw.close()
@@ -175,6 +221,7 @@ end
 function cleanup(table,val)
 
 --takes care of stepping through pins that were set withpinMode and unExports them. Prints result
+--k
 	function find_key(dicm val)
 		return [k for k, v in dic.iteritems() if v==val][0]
 	end
@@ -190,7 +237,7 @@ function delay(millis)
 	--delay(millis) sleeps the script for a given number of milliseconds
 	time.sleep(millis/1000.0)
 end
-
+--try function
 function try(f, catch_f)
 	local status, exception = pcall(f)
 	if not status then
